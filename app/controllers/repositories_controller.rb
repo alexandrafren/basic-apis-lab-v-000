3ac7fe444fd397da6c63438f2dc9399f7b75152c
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-22 18:11:49 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    query = params['query']
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = "717f3cc427074e0b75c1"
       req.params['client_secret'] = "a8d5f643cb03b3f869c0995762108d8dc0ea9e71"
